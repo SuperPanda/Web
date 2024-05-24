7c607a4cad50fdeb171fadedf0dc737a824d0766
Andrew Briscoe <SuperPanda@users.noreply.github.com>
2024-05-24 13:56:23 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,14 +2,14 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open('v1').then(cache => {
       return cache.addAll([
-        '/Web/',
-        '/Web/app.html',
-        '/Web/manifest.json',
-        '/Web/favicon.ico',
-        '/Web/favicon-16x16.png',
-        '/Web/favicon-32x32.png',
-        '/Web/icon-192x192.png',
-        '/Web/icon-512x512.png',
+        '/web/',
+        '/web/app.html',
+        '/web/manifest.json',
+        '/web/favicon.ico',
+        '/web/favicon-16x16.png',
+        '/web/favicon-32x32.png',
+        '/web/icon-192x192.png',
+        '/web/icon-512x512.png',
       ]);
     })
   );
